8936689c10dff2546b3f51d8673268244deb8bbe
Andrew Dellaringa <dellaringa@ibm.com>
2026-07-21 13:41:11 -0700

    Removed the timeouts set from earlier lessons and funished UI componenet testing also fixed tsconfig as changing to bundler from node was causing an error

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,11 +12,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout : 10000, // 10,000 milliseconds manual timeout time global settings
-  globalTimeout: 60000,
-  expect: {
+  // timeout : 10000, // 10,000 milliseconds manual timeout time global settings
+  // globalTimeout: 60000,
+  /*expect: {
     timeout: 2000
-  },
+  },*/
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -37,8 +37,8 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     //setting timeouts
-    actionTimeout: 5000,
-    navigationTimeout: 5000
+    // actionTimeout: 5000,
+    // navigationTimeout: 5000
   },
 
   /* Configure projects for major browsers */
