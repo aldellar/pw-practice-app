e2f6a6c9fdd2dc22c8d95149727faa32994cbf73
Andrew Dellaringa <dellaringa@ibm.com>
2026-07-20 17:28:53 -0700

    Finished the timeout lesson

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  //timeout : 10000, 10,000 milliseconds manual timeout time
+  timeout : 10000, // 10,000 milliseconds manual timeout time global settings
+  globalTimeout: 60000,
+  expect: {
+    timeout: 2000
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,6 +35,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    //setting timeouts
+    actionTimeout: 5000,
+    navigationTimeout: 5000
   },
 
   /* Configure projects for major browsers */
--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,9 @@
 import {test, expect} from '@playwright/test'
 
-test.beforeEach(async({page}) => {
+test.beforeEach(async({page}, testInfo) => {
     await page.goto('http://uitestingplayground.com/ajax')
     page.getByText('Button Triggering Ajax Request').click()
+    testInfo.setTimeout(testInfo.timeout + 2000) //updatin timeout before each 
 })
 
 test('auto waiting', async({page}) => {
@@ -16,7 +17,7 @@ test('auto waiting', async({page}) => {
 
     //  expect(text).toContain('Data loaded with AJAX get request.')
 
-    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000}) //set timeout inside of the assertion
 
 })
 
@@ -31,8 +32,16 @@ test('alternative waits', async ({page}) => {
 
     // __ wait for network calls to be completed ('NOT RECOMMENDED')
     await page.waitForLoadState('networkidle')
-
+    
     const text = await successButton.allTextContents()  //this will fail without some wait put in place 
     expect(text).toContain('Data loaded with AJAX get request.')
 
+})
+
+test('timeouts', async ({page}) => {
+    // test.setTimeout(10000) // set the global timeout 
+    //test.slow() this multiplies the global test default so this will run for longer
+    const successButton = page.locator('.bg-success')
+    // await successButton.click({timeout: 16000}) //this will overwright any other timeouts set in playwright code
+    await successButton.click()
 })
