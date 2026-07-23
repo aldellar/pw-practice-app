c53dd1a2801dfc36ea52a6a017f533b36fba83c2
Andrew Dellaringa <dellaringa@ibm.com>
2026-07-23 14:08:51 -0700

    finished the locators in page object lesson

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,33 +1,43 @@
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 
 export class NavigationPage {
     readonly page: Page
+    readonly formLayoutsMenuItem: Locator
+    readonly datePickerMenuItem: Locator
+    readonly smartTableMenuItem: Locator    //you can do this but honestly it would be best to keep the locator inside of the tests
+    readonly toastrMenuItem: Locator
+    readonly tooltipMenuItem: Locator
 
     constructor(page: Page){
         this.page = page
+        this.formLayoutsMenuItem = page.getByText('Form Layouts')
+        this.datePickerMenuItem = page.getByText('Datepicker')
+        this.smartTableMenuItem = page.getByText('Smart Table')
+        this.toastrMenuItem = page.getByText('Toastr')
+        this.tooltipMenuItem = page.getByText('Tooltip')
     }
 
     async formLayoutsPage(){
-        await this.selectGroupMenuItem('Forms')
-        await this.page.getByText('Form Layouts').click()
+        await this.selectGroupMenuItem('Forms') // you can keep the locator inside here that way it makes it more simple 
+        await this.formLayoutsMenuItem.click()
     }
 
     async datePickerPage(){
         await this.selectGroupMenuItem('Forms')
-        await this.page.getByText('Datepicker').click()
+        await this.datePickerMenuItem.click()
     }
 
     async smartTablePage(){
         await this.selectGroupMenuItem('Tables & Data')
-        await this.page.getByText('Smart Table').click()
+        await this.smartTableMenuItem.click()
     }
     async toastrPage(){
         await this.selectGroupMenuItem('Modal & Overlays')
-        await this.page.getByText('Toastr').click()
+        await this.toastrMenuItem.click()
     }
     async tooltipPage(){
         await this.selectGroupMenuItem('Modal & Overlays')
-        await this.page.getByText('Tooltip').click()
+        await this.tooltipMenuItem.click()
     }
 
     private async selectGroupMenuItem(groupItemTitle: string){
