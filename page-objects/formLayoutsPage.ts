7f548d60e825badec040f4817ebbfd8efb6722aa
Andrew Dellaringa <dellaringa@ibm.com>
2026-07-23 14:52:46 -0700

    finished form layouts lesson

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,30 @@
+import { Page } from '@playwright/test'
+import { using } from 'rxjs'
+
+export class FormLayoutsPage {
+
+    private readonly page: Page
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    async submitUsingTheGridFormWithCredentialsAndSelectOption(email: string, password: string, optionText: string){
+        const usingTheGridForm = this.page.locator('nb-card', {hasText: "Using the Grid"})
+        await usingTheGridForm.getByRole('textbox', {name: "Email"}).fill(email)
+        await usingTheGridForm.getByRole('textbox', {name: "Password"}).fill(password)
+        await usingTheGridForm.getByRole('radio', {name: optionText}).check({force: true})      //find it using force here if it is hidden
+        await usingTheGridForm.getByRole('button').click()
+
+    }
+
+    async submitInLineFormWithNameEmailAndCheckbox(name: string, email: string, rememberMe: boolean){
+        const inlineForm = this.page.locator('nb-card', {hasText: "Inline form"})
+        await inlineForm.getByRole('textbox', {name: "Jane Doe"}).fill(name)
+        await inlineForm.getByRole('textbox', {name: "Email"}).fill(email)
+        if(rememberMe){
+            await inlineForm.getByRole('checkbox').check({force: true})
+        }
+        await inlineForm.getByRole('button').click()
+    }
+}
