4edae796e4a70083bf254d3c42fab88a9b5c1dbb
Andrew Dellaringa <dellaringa@ibm.com>
2026-07-30 14:49:00 -0700

    one more conditional thing for the mobile lesson

--- tests/testMobile.spec.ts
+++ tests/testMobile.spec.ts
@@ -3,9 +3,16 @@
     
     test('input fields', async({page}, testInfo) => {
         await page.goto('/')
-        await page.locator('.sidebar-toggle').click()
+        if(testInfo.project.name == 'mobile'){
+            await page.locator('.sidebar-toggle').click()
+
+        }
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
+           if(testInfo.project.name == 'mobile'){
+            await page.locator('.sidebar-toggle').click()
+
+        }
         const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
 
         await usingTheGridEmailInput.fill('test@test.com')
