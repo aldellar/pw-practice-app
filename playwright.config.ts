b205bc8aa6b232a7d4f3f207d51aedc93e8c742e
Andrew Dellaringa <dellaringa@ibm.com>
2026-07-30 14:45:57 -0700

    finished device emulator lesson

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,6 +63,13 @@ export default defineConfig<TestOptions>({
           viewport: {width: 1920, height: 1080}
         }
     },
+    {
+        name: 'mobile',
+        testMatch: '**/testMobile.spec.ts',
+        use: {
+          ...devices['iPhone 13 Pro']
+        }
+    },
   ],
 
 
